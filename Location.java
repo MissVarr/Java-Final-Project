93ca55545c70a6170e77eba978d23572b6d12ab8
lebronfan744 <vikramsrinath7@gmail.com>
2017-05-31 12:07:45 -0400

    Create Location.java

---
+++ Location.java
@@ -0,0 +1,20 @@
+public class Location
+{
+   private int x;
+	 private int y;
+   public Location (int x1, int y1)
+	 {
+	   x = x1;
+		 y = y1;
+	 }
+	 
+	 public int getX()
+	 {
+	    return x; 
+	 }
+	 
+	 public int getY()
+	 {
+	    return y; 
+	 }
+}
