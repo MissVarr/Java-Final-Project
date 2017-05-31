ed83d1687d16faa2d11ca3dc0bdbdc0227717b65
lebronfan744 <vikramsrinath7@gmail.com>
2017-05-31 12:12:50 -0400

    Update Location.java

--- Location.java
+++ Location.java
@@ -3,18 +3,18 @@ public class Location
    private int x;
    private int y;
    public Location (int x1, int y1)
-	 {
+   {
 	   x = x1;
 	   y = y1;
-	 }
+   }
 	 
-	 public int getX()
-	 {
-	    return x; 
-	 }
+   public int getX()
+   {
+	   return x; 
+   }
 	 
-	 public int getY()
-	 {
-	    return y; 
-	 }
+   public int getY()
+   {
+	   return y; 
+   }
 }
