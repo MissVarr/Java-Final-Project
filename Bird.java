8bd73a16ab6f2b417e8b82793c15c5fce9ab1948
rishsol <rishabsolanki09@gmail.com>
2017-06-03 16:41:30 -0400

    Create Bird.java

---
+++ Bird.java
@@ -0,0 +1,24 @@
+import java.awt.Event;
+import java.awt.event.ActionListener;
+
+public class Bird extends Obstacle implements ActionListener
+{
+	public Bird(location l)
+	{
+	  loc = l;
+	  length = someNum;
+	  width  = someNum;
+	}
+	public Location moveDown()
+	{
+	  
+	}
+	public void move()
+	{
+	  
+	}
+	public void actionPerformed(Event e)
+	{
+		
+	}
+}
