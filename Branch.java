6b1e93d252a21a9fe7c85e770d64710f6d9ee457
rishsol <rishabsolanki09@gmail.com>
2017-06-03 16:38:04 -0400

    Create Branch.java

---
+++ Branch.java
@@ -0,0 +1,27 @@
+import java.awt.Event;
+import java.awt.event.ActionListener;
+
+public class Branch extends Obstacle implements ActionListener
+{ 
+  public Branch(Location l)
+  {
+	loc = l;
+	collided = false;
+	length = someNum
+	width = someNum
+  }
+  
+  public Location returnLocation()
+  {
+	return loc;
+  }
+  public Location moveDown()
+  {
+	//move down
+    return loc;
+  }
+  public void actionPerformed(Event e)
+  {
+	//stuff: moveDown?
+  }
+ 
