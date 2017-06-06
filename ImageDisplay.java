bd8f1abac962e57235a4853a9bf597104323d6d7
rishsol <rishabsolanki09@gmail.com>
2017-06-06 14:13:38 -0100

    Update ImageDisplay.java

--- ImageDisplay.java
+++ ImageDisplay.java
@@ -1,45 +1,41 @@
 import java.awt.Graphics;
 import java.awt.Image;
+import javax.swing.JFrame;
+import javax.imageio.ImageIO;
 import java.io.File;
 import java.io.IOException;
 
-import javax.imageio.ImageIO;
-import javax.swing.JFrame;
+public class ImageDisplay extends JFrame {
+	private Image picture;
 
-public class ImageDisplay extends JFrame 
-{
-  private Image img;
-  
-  public ImageDisplay()
-  {
-	 super("Image Test");
-	 setSize(1000,800);
-	 String pathname = "images/bird.png";
-	 try
-	 {
-	  img = ImageIO.read(new File(pathname));
-	 }
-	 catch(IOException ex)
-	 {
-		System.out.print("Can't load");
-	 }
-   }
-  public void paint(Graphics g)
-  {
-	super.paint(g);
-	if(img != null)
-	{
-	  int x = (getWidth() - img.getWidth(null))/2;
-	  int y = (getHeight() - img.getHeight(null)) / 2;
-	  g.drawImage(img, x, y, null);
+	public ImageDisplay() {
+		super("Image Test");
+		setSize(1000, 800);
+
+		String pathname = "P:\\My Pictures\\bird.jpg";
+		try {
+			picture = ImageIO.read(new File(pathname));
+		} catch (IOException ex) {
+			System.out.println("*** Can't load " + pathname + " ***");
+			System.exit(1);
+		}
 	}
-  }
-  public static void main(String args[])
-  {
-	ImageDisplay window = new ImageDisplay();
-	window.setLocation(20,20);
-	window.setDefaultCloseOperation(EXIT_ON_CLOSE);
-	window.setVisible(true);
+
+	public void paint(Graphics g) {
+		super.paint(g);
+
+		if (picture != null) {
+			// center the picture:
+			int x = (getWidth() - picture.getWidth(null)) / 2;
+			int y = (getHeight() - picture.getHeight(null)) / 2;
+			g.drawImage(picture, x, y, null);
+		}
 	}
-  }
-  
+
+	public static void main(String[] args) {
+		ImageDisplay window = new ImageDisplay();
+		window.setLocation(20, 20);
+		window.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		window.setVisible(true);
+	}
+}
