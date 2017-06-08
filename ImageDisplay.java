6a7d572a0edc85c1f18e012bfc30bf01aa344576
rishsol <rishabsolanki09@gmail.com>
2017-06-08 16:19:15 -0100

    Create ImageDisplay.java

--- ImageDisplay.java
+++ ImageDisplay.java
@@ -1,41 +1,68 @@
-import java.awt.Graphics;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 import java.awt.Image;
-import javax.swing.JFrame;
-import javax.imageio.ImageIO;
-import java.io.File;
 import java.io.IOException;
+import java.net.URL;
+import javax.imageio.ImageIO;
 
-public class ImageDisplay extends JFrame {
-	private Image picture;
+public class ImageDisplay extends JPanel implements ActionListener
+{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	//Banana b1;
+	Bird b1;
+	Timer time;
+	//boolean collision = false;
 
-	public ImageDisplay() {
-		super("Image Test");
-		setSize(1000, 800);
-
-		String pathname = "P:\\My Pictures\\bird.jpg";
-		try {
-			picture = ImageIO.read(new File(pathname));
-		} catch (IOException ex) {
-			System.out.println("*** Can't load " + pathname + " ***");
-			System.exit(1);
-		}
+	public ImageDisplay() 
+	{
+		//b1 = new Banana(350, 100);
+		b1 = new Bird (450, 0);
+		time = new Timer(1, this);
+		time.start();
 	}
 
-	public void paint(Graphics g) {
+	public void actionPerformed(ActionEvent e) 
+	{
+		//b1.move();
+		b1.move();
+		//checkCollision();
+		repaint();
+	}
+
+	public void paint(Graphics g) 
+	{
+		Image image = null;
+        try {
+            URL url = new URL("http://www.pngmart.com/files/1/Banana-PNG.png");
+            image = ImageIO.read(url);
+        } catch (IOException e) {
+        	e.printStackTrace();
+        }
 		super.paint(g);
+		Graphics2D g2d = (Graphics2D) g;
+		g2d.drawImage(image, b1.x, b1.y, 75, 50, null);
+		//g2d.setColor(Color.YELLOW);
+		//g2d.fillRect(b1.x, b1.y, 50, 50);
+		//g2d.setColor(Color.GREEN);
+		//g2d.fillRect(b2.x, b2.y, 50, 50);
+		//if (collision)
+			//g2d.drawString("COLLISION", 350, 50);
+	}
 
-		if (picture != null) {
-			// center the picture:
-			int x = (getWidth() - picture.getWidth(null)) / 2;
-			int y = (getHeight() - picture.getHeight(null)) / 2;
-			g.drawImage(picture, x, y, null);
+	/*public void checkCollision() 
+	{
+		Rectangle ban1 = b1.bounds();
+		Rectangle ban2 = b2.bounds();
+		if (ban1.intersects(ban2))
+		{
+			collision = true;
 		}
-	}
-
-	public static void main(String[] args) {
-		ImageDisplay window = new ImageDisplay();
-		window.setLocation(20, 20);
-		window.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		window.setVisible(true);
-	}
-}
+		else
+		{
+			collision = false;
+		}
+	}*/
