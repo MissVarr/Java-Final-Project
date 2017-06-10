b2bc8eb6cb61345caf4347c4c166244a2fc53f88
lebronfan744 <vikramsrinath7@gmail.com>
2017-06-10 18:39:46 -0400

    Create Menu.java

---
+++ Menu.java
@@ -0,0 +1,93 @@
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+import javax.imageio.ImageIO;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.awt.event.ActionEvent;
+import java.awt.Font;
+import java.awt.Image;
+import javax.swing.JLabel;
+import java.awt.Color;
+import com.jgoodies.forms.factories.DefaultComponentFactory;
+import javax.swing.JTextPane;
+import javax.swing.JMenuBar;
+
+public class Menu {
+
+	private JFrame frmJ;
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Menu window = new Menu();
+					window.frmJ.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Menu() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frmJ = new JFrame("MONKEY MAYHEM");
+		URL url = null;
+		Image img = null;
+		try {
+			url = new URL("https://ak5.picdn.net/shutterstock/videos/7205341/thumb/1.jpg?i10c=img.resize(height:160)");
+			img = ImageIO.read(url.openStream());
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		frmJ.setContentPane(new JLabel(new ImageIcon(img)));
+		frmJ.setBounds(100, 100, 700, 500);
+		frmJ.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JButton btnPlayNow = new JButton("Play Now!");
+		btnPlayNow.setForeground(new Color(51, 153, 102));
+		btnPlayNow.setFont(new Font("Papyrus", Font.PLAIN, 30));
+		btnPlayNow.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		
+		JButton btnInstructions = new JButton("Instructions");
+		btnInstructions.setForeground(new Color(51, 153, 51));
+		btnInstructions.setFont(new Font("Papyrus", Font.PLAIN, 30));
+		GroupLayout groupLayout = new GroupLayout(frmJ.getContentPane());
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(235)
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addComponent(btnPlayNow, GroupLayout.PREFERRED_SIZE, 192, GroupLayout.PREFERRED_SIZE)
+						.addComponent(btnInstructions, GroupLayout.PREFERRED_SIZE, 192, GroupLayout.PREFERRED_SIZE))
+					.addContainerGap(257, Short.MAX_VALUE))
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
+					.addContainerGap(174, Short.MAX_VALUE)
+					.addComponent(btnPlayNow, GroupLayout.PREFERRED_SIZE, 74, GroupLayout.PREFERRED_SIZE)
+					.addGap(71)
+					.addComponent(btnInstructions, GroupLayout.PREFERRED_SIZE, 74, GroupLayout.PREFERRED_SIZE)
+					.addGap(68))
+		);
+		frmJ.getContentPane().setLayout(groupLayout);
+	}
+}
