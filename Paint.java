288a7f2dddd63be701e7720c6da1f1c2dfd28f16
lebronfan744 <vikramsrinath7@gmail.com>
2017-06-11 15:39:11 -0400

    Create Paint.java

--- Paint.java
+++ Paint.java
@@ -5,36 +5,53 @@ import javax.swing.*;
 public class Paint extends JPanel implements ActionListener
 {
 	//Banana b1;
-	Banana b2;
-	Timer time;
+	private Banana b2;
+	private Timer time;
 	//boolean collision = false;
 
+	private enum STATE
+	{
+		MENU,
+		GAME
+	};
+	
+	private STATE State = STATE.MENU;
+	
 	public Paint() 
 	{
-		//b1 = new Banana(350, 100);
-		b2 = new Banana(350, 300);
-		time = new Timer(1, this);
-		time.start();
+		if (State == STATE.GAME)
+		{
+			//b1 = new Banana(350, 100);
+			b2 = new Banana(350, 300);
+			time = new Timer(1, this);
+			time.start();
+		}
 	}
 
 	public void actionPerformed(ActionEvent e) 
 	{
-		//b1.move();
-		b2.move();
-		//checkCollision();
-		repaint();
+		if (State == STATE.GAME)
+		{
+			//b1.move();
+			b2.move();
+			//checkCollision();
+			repaint();
+		}
 	}
 
 	public void paint(Graphics g) 
 	{
-		super.paint(g);
-		Graphics2D g2d = (Graphics2D) g;
-		g2d.setColor(Color.YELLOW);
-		//g2d.fillRect(b1.x, b1.y, 50, 50);
-		//g2d.setColor(Color.GREEN);
-		g2d.fillRect(b2.x, b2.y, 50, 50);
-		//if (collision)
+		if (State == STATE.GAME)
+		{
+			super.paint(g);
+			Graphics2D g2d = (Graphics2D) g;
+			g2d.setColor(Color.YELLOW);
+			//g2d.fillRect(b1.x, b1.y, 50, 50);
+			//g2d.setColor(Color.GREEN);
+			g2d.fillRect(b2.x, b2.y, 50, 50);
+			//if (collision)
 			//g2d.drawString("COLLISION", 350, 50);
+		}
 	}
 
 	/*public void checkCollision() 
