3b11f5eb10e5427f116668ac1e810967656f5d0d
aryanmalesha <aryanmalesha@gmail.com>
2017-06-14 16:49:28 -0700

    Create Paint.java

--- Paint.java
+++ Paint.java
@@ -1,72 +1,62 @@
 import java.awt.*;
 import java.awt.event.*;
+import java.util.ArrayList;
 import javax.swing.*;
 
 public class Paint extends JPanel implements ActionListener
 {
-	//Banana b1;
-	private Banana b2;
-	private Timer time;
-	//boolean collision = false;
+		ArrayList<Actor> actors = new ArrayList<Actor>();
+		Timer time1;
+		int count = 0;
 
-	private enum STATE
-	{
-		MENU,
-		GAME
-	};
-	
-	private STATE State = STATE.MENU;
-	
-	public Paint() 
-	{
-		if (State == STATE.GAME)
+		public Paint() 
 		{
-			//b1 = new Banana(350, 100);
-			b2 = new Banana(350, 300);
-			time = new Timer(1, this);
-			time.start();
+			actors.add(new Branch(100, -200, 150, 50));
+			actors.add(new Banana(475, -500, 50, 50));
+			actors.add(new Branch(750, -800, 150, 50));
+			time1 = new Timer(5, this);
+			time1.start();
+			
 		}
-	}
 
-	public void actionPerformed(ActionEvent e) 
-	{
-		if (State == STATE.GAME)
-		{
-			//b1.move();
-			b2.move();
-			//checkCollision();
+		public void actionPerformed(ActionEvent e) 
+		{		
+			if (actors.get(count).getY() == 1000)
+			{
+				int rnd = (int)((Math.random() * 2) + 1);
+				
+				if(rnd == 1)
+				{
+					actors.add(new Branch(100, -200, 150, 50));
+					actors.add(new Banana(475, -500, 50, 50));
+					actors.add(new Branch(750, -800, 150, 50));	
+				}
+				else if(rnd == 2)
+				{
+					actors.add(new Branch(100, -200, 150, 50));
+					actors.add(new Banana(475, -500, 50, 50));
+					actors.add(new Branch(100, -800, 150, 50));
+				}
+				
+				count += 3;
+			}
+			
+			for(Actor a: actors)
+			{
+				a.move();
+			}
 			repaint();
 		}
-	}
 
-	public void paint(Graphics g) 
-	{
-		if (State == STATE.GAME)
+		public void paint(Graphics g) 
 		{
 			super.paint(g);
 			Graphics2D g2d = (Graphics2D) g;
-			g2d.setColor(Color.YELLOW);
-			//g2d.fillRect(b1.x, b1.y, 50, 50);
-			//g2d.setColor(Color.GREEN);
-			g2d.fillRect(b2.x, b2.y, 50, 50);
-			//if (collision)
-			//g2d.drawString("COLLISION", 350, 50);
+			
+			for (Actor a: actors)
+			{
+				g2d.setColor(Color.YELLOW);
+				g2d.fillRect(a.getX(), a.getY(), a.getLength(), a.getWidth());
+			}
 		}
-	}
-
-	/*public void checkCollision() 
-	{
-		Rectangle ban1 = b1.bounds();
-		Rectangle ban2 = b2.bounds();
-
-		if (ban1.intersects(ban2))
-		{
-			collision = true;
-		}
-		else
-		{
-			collision = false;
-		}
-	}*/
-
 }
