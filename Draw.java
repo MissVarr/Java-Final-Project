0377a4950238cfb1525d2d299ef5d8d46289769f
lebronfan744 <vikramsrinath7@gmail.com>
2017-06-14 21:45:49 -0400

    Update Draw.java

--- Draw.java
+++ Draw.java
@@ -2,12 +2,30 @@ import javax.swing.*;
 
 public class Draw 
 {
+	private enum STATE{
+		MENU,
+		GAME
+	};
+	
+	private static STATE State = STATE.MENU;
+	
 	public static void main(String[] args) 
 	{
-		JFrame frame = new JFrame("Collision Detection");
-		frame.setVisible(true);
-		frame.setSize(1000, 1000);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.add(new Paint());
+		Menu.display();
+	}
+	
+	public static void changeState(){
+		if(State == STATE.MENU){
+			State = STATE.GAME;
+			JFrame frame = new JFrame("Collision Detection");
+			frame.setVisible(true);
+			frame.setSize(1000, 1000);
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			frame.add(new Paint());
+		}
+		else{
+			State = STATE.MENU;
+			Menu.display();
+		}
 	}
 }
