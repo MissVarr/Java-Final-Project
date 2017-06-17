cada0c98dab7954e164f46261ede39e1fe2c2053
aryanmalesha <aryanmalesha@gmail.com>
2017-06-17 16:44:52 -0700

    Update Paint.java

--- Monkey Mayhem/src/Paint.java
+++ Monkey Mayhem/src/Paint.java
@@ -1,22 +1,36 @@
 import java.awt.*;
+
 import java.awt.event.*;
 import java.util.ArrayList;
 import javax.swing.*;
 
+//The paint class handles overall game play
+
 public class Paint extends JPanel implements ActionListener
 {
+		//List of all actors on the field except for the monkey
 		ArrayList<Actor> actors = new ArrayList<Actor>();
+		
 		Timer time1;
+		
+		//Variable used to ensure that objects are properly added to the list
 		int count = 0;
+		
 		Monkey m;
+		
+		//Incremented every time the monkey intersects with a banana
 		int score;
-
+		
+		//Constructor
 		public Paint() 
 		{
+			//Adds initial objects to list
 			actors.add(new Branch(100, -200));
 			actors.add(new Banana(475, -500));
 			actors.add(new Branch(750, -800));
 			m = new Monkey(100, 800);
+			
+			//Starts the timer which will run the ActionPerformed method 200 times a second
 			time1 = new Timer(5, this);
 			time1.start();
 		}
@@ -27,9 +41,10 @@ public class Paint extends JPanel implements ActionListener
 			
 			if (m.isAlive)
 			{
+				//If the first obstacle in a 'set' hits the bottom of the screen
 				if (actors.get(count % 3).getY() == 1000)
 				{	
-					System.out.println(actors);
+					//generate a random number to choose the next panel
 					int rnd = (int)((Math.random() * 6) + 1);
 					if(rnd == 1)
 					{
@@ -70,25 +85,29 @@ public class Paint extends JPanel implements ActionListener
 
 				}
 				
+				//Cycle through every Actor in the list and remove those that have passed the bottom of the screen
 				for(int i = 0; i < actors.size(); i++)
 				{
 					if(actors.get(i).getY() >= 1000)
 					{
 						actors.remove(i);
 						i--;
+						//add 1 to count so the next set is made at the right time
 						count ++;
 					}
 				}
 				
+				//Check if a collision is occurring
 				this.collisionDetection();
 				
+				//Move every actor in the list and repaint them in the new positions
 				for(Actor a: actors)
 				{
 					a.move();
 				}
 				repaint();
 			}
-			else
+			else //if it is not alive, call the paint method
 			{
 				 repaint();
 			}
@@ -102,7 +121,7 @@ public class Paint extends JPanel implements ActionListener
 			
 			if(m.isAlive == false)
 			{
-                g2d.drawString("COLLISION", 350, 50);
+                g2d.drawString("GAME IS OVER", 500, 700);
 
 			}
 			
@@ -115,9 +134,14 @@ public class Paint extends JPanel implements ActionListener
 				g2d.fillRect(a.getX(), a.getY(), a.getLength(), a.getWidth());
 			}
 		}
+		
+		//check if collision is occurring
 		public void collisionDetection()
 		{
+			//create a rectangle hitbox for the monkey
 			Rectangle rec1 = m.hitBox();
+			
+			//go through each actor and create a hitbox --> check if they intersect and act appropriately
 			for (Actor a: actors)
 			{
 				Rectangle rec2 = a.hitBox();
@@ -133,9 +157,5 @@ public class Paint extends JPanel implements ActionListener
 					}
 				}
 			}
-		}
-		public int getScore()
-		{
-			return score;
 		}
 }
