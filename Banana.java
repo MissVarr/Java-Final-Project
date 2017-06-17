ba2f4e0ecfbb5359cbfae17be5017f05da89630a
aryanmalesha <aryanmalesha@gmail.com>
2017-06-16 19:52:35 -0700

    Update Banana.java

--- Banana.java
+++ Banana.java
@@ -1,43 +1,14 @@
-import java.awt.Rectangle;
-
-public class Banana 
-{
-	int x, y;
-	boolean down = false;
-	
+public class Banana extends Actor
+{	
 	public Banana(int startX, int startY)
 	{
 		x = startX;
 		y = startY;
+		length = 50;
+		width = 50;
 	}
-	
 	public void move()
 	{
-		y++;
-		
-		/*if (y == 100)
-		{
-			down = true;
-		}
-		if (y == 300)
-		{
-			down = false;
-		}
-		if (down)
-		{
-			y--;
-		}
-		else
-		{
-			y++;
-		}*/
+		y+= 1;
 	}
-	
-	public Rectangle bounds()
-	{
-		return (new Rectangle(x, y, 50, 50));
-	}
-	
-	
-
 }
