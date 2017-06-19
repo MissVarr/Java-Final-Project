cb53d2badbf3ae8baad5cce5f759d43018104101
lebronfan744 <vikramsrinath7@gmail.com>
2017-06-18 21:08:12 -0400

    Update Actor.java

--- Monkey Mayhem/src/Actor.java
+++ Monkey Mayhem/src/Actor.java
@@ -1,35 +1,73 @@
+import java.awt.Color;
 import java.awt.Rectangle;
+
+//The Actor class is a superclass that provides code for all objects on the field 
+//This includes the monkey, bananas, branches, and snakes
 
 public abstract class Actor
 {
+	//fields
+	private int x;
+	private int y;
+	private int length;
+	private int width;
+	private Color color;
 	
-	int x, y, length, width;
+	//constructors
+	public Actor(int x1, int y1, int l, int w, Color c)
+	{
+		x = x1;
+		y = y1;
+		length = l;
+		width = w;
+		color = c;
+	}
+	
+	//Methods
+	
+	
+	//Alter the x or y coordinates of the Actors
 	
 	public abstract void move();
 	
-	public int getX()
+	public void shift(int nx, int ny)
 	{
-		return x;
+		x+= nx;
+		y+= ny;
 	}
 	
-	public int getY()
-	{
-		return y;
-	}
-	
-	public int getLength()
-	{
-		return length;
-	}
-	
-	public int getWidth()
-	{
-		return width;
-	}
-	
+	//Creates a hitbox that can be used for collision detection
 	public Rectangle hitBox()
 	{
 		return (new Rectangle(x, y, length, width));
 	}
 	
+	//Resets size of actor
+	public void reset()
+	{
+		length = 0;
+		width = 0;
+	}
+	
+	//Accessor Methods
+	public int getX()
+	{
+		return x;
+	}
+	public int getY()
+	{
+		return y;
+	}
+	public int getLength()
+	{
+		return length;
+	}
+	public int getWidth()
+	{
+		return width;
+	}
+	public Color getColor()
+	{
+		return color;
+	}
 }
